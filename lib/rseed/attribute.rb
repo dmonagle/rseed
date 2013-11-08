d28d9d875c68dce136c5dc4dcaa414a3ac4c80c7
David Monagle <david.monagle@intrica.com.au>
2013-11-08 15:52:39 +1100

    model attribute is optional now as long as model is set or true

--- lib/rseed/attribute.rb
+++ lib/rseed/attribute.rb
@@ -25,13 +25,14 @@ module Rseed
       return nil if values[self.name].nil?
       value = values[self.name]
 
-      if options[:model_attribute]
+      if options[:model_attribute] || options[:model]
         model = options[:model] || true
+        model_attribute = options[:model_attribute] || :id
         # The attribute is a model, we look up the model via the specified attribute
         model_name = model == true ? self.name : model
         klass = model_name.is_a?(Class) ? model_name : model_name.to_s.classify.constantize
         model_match = options[:model_match] || :first
-        value = klass.where(options[:model_attribute] => value).send(model_match.to_s)
+        value = klass.where(model_attribute => value).send(model_match.to_s)
       elsif options[:type]
         # Check for a deserialize function for the type
         dsf = "deserialize_#{options[:type].to_s}"
